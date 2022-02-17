f83ee78af288d2c3cab6de8fe76fab4460de7463
Pedro Daniel <pedro-edu19@outlook.com>
2022-02-17 17:54:12 -0300

    create sliders from projects

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,19 @@
  AOS.init({
     duration:1000,
  });
+
+ let cont = 0;
+
+function sliderShow () {
+   
+   setInterval(() =>{
+      document.querySelector('.sliders').style.marginLeft = `${cont}px`;
+      cont -= 405;
+      if(cont == -1620){
+         cont = 0
+      }
+   }, 3000);
+   
+}
+
+sliderShow();
